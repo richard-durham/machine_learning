e487b17800b411344a645e08d579df94c8546cea
Richard Durham <richard_durham@me.com>
2017-01-17 20:33:10 +0000

    Add linear regression example

---
+++ brain_body_regression.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn import linear_model
+import numpy as np 
+import matplotlib.pyplot as plt
+
+#read data
+dataframe = pd.read_fwf('brain_body.txt')
+x_values = dataframe[['Brain']]
+y_values = dataframe[['Body']]
+
+#train model
+body_reg = linear_model.LinearRegression()
+body_reg.fit(x_values, y_values)
+
+#visualize results
+plt.scatter(x_values, y_values)
+plt.pyplot(x_values, body_reg.predict(x_values))
+plt.show()
